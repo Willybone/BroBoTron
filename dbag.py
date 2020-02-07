7059fe88fe41cb46b60e98dddc29cb2dfa2c2ecf
Willy Bone <willybone@gmail.com>
2020-02-07 09:10:33 -0500

    Realigning

--- dbag.py
+++ dbag.py
@@ -8,20 +8,20 @@ class Dbag(Sprite):
 		super(Dbag, self).__init__()
 		self.screen = screen
 		self.settings = settings
-		
+
 		dbag_selector = randint(1,4)
 		if dbag_selector == 1:
-			self.image = pygame.image.load('images/squirt.png')
+			self.image = pygame.image.load('images/squirt.bmp')
 		elif dbag_selector == 2:
-			self.image = pygame.image.load('images/beedrill.png')
+			self.image = pygame.image.load('images/beedrill.bmp')
 		elif dbag_selector == 3:
-			self.image = pygame.image.load('images/bulbasaur.png')				
+			self.image = pygame.image.load('images/bulbasaur.bmp')
 		elif dbag_selector == 4:
-			self.image = pygame.image.load('images/koffing.png')
-		
+			self.image = pygame.image.load('images/koffing.bmp')
+
 		self.rect = self.image.get_rect()
 		self.screen_rect = screen.get_rect()
-		
+
 		self.side_select = randint(1,4)
 		if self.side_select == 1:
 			self.rect.centerx = randint(self.screen_rect.left + 5, self.screen_rect.right - 5)
@@ -35,22 +35,22 @@ class Dbag(Sprite):
 		if self.side_select == 4:
 			self.rect.right = 0
 			self.rect.centery = randint(self.screen_rect.top + 5, self.screen_rect.bottom - 5)
-						
+
 		self.xdec = float(self.rect.x)
 		self.ydec = float(self.rect.y)
-		
+
 		self.xmove = 0
 		self.ymove = 0
 		self.step_count = 0
-		
+
 	def update(self, settings, screen, stats, dude):
 		if self.step_count >= settings.dbag_max_steps:
 			self.update_direction(dude)
 		else:
 			self.step_count += 1
-		self.xdec += (self.xmove * (settings.dbag_speed_factor + 
+		self.xdec += (self.xmove * (settings.dbag_speed_factor +
 			(stats.round_num * settings.round_speed_increase)))
-		self.ydec += (self.ymove * (settings.dbag_speed_factor + 
+		self.ydec += (self.ymove * (settings.dbag_speed_factor +
 			(stats.round_num * settings.round_speed_increase)))
 		self.rect.x = self.xdec
 		self.rect.y = self.ydec
@@ -63,12 +63,10 @@ class Dbag(Sprite):
 			self.xmove = 1
 		else:
 			self.xmove = -1
-		
+
 		if dude.rect.centery > self.rect.centery:
 			self.ymove = 1
 		else:
 			self.ymove = -1
-		
+
 		self.step_count=0
-			
-		
--- dude.py
+++ dude.py
@@ -7,31 +7,31 @@ class Dude(Sprite):
 		super(Dude, self).__init__()
 		self.screen = screen
 		self.settings = settings
-		
+
 		# Get dude image and get its rect.
-		self.image = pygame.image.load('images/dude.png')
+		self.image = pygame.image.load('images/dude.bmp')
 		self.rect = self.image.get_rect()
 		self.screen_rect = screen.get_rect()
-		
+
 		# Start new dude at the bottom center
 		self.rect.centerx = self.screen_rect.centerx
 		self.rect.centery = self.screen_rect.centery
-		
+
 		# Creating decimal versions of the dude's position for fine control of speed
 		self.centerxdec = float(self.rect.centerx)
 		self.centerydec = float(self.rect.centery)
-		
+
 		# Setting the action flags
 		self.moving_x = 0.0
 		self.moving_y = 0.0
-		
+
 	def center_dude(self):
 		self.centerxdec = self.screen_rect.right / 2
-		self.centerydec = self.screen_rect.bottom / 2		
-		
+		self.centerydec = self.screen_rect.bottom / 2
+
 	def blitme(self):
-		""" Draw the dude at its current location 
-		Note that I'm setting the real rect coordinates to the 
+		""" Draw the dude at its current location
+		Note that I'm setting the real rect coordinates to the
 		rounded float values"""
 		self.rect.centerx = self.centerxdec
 		self.rect.centery = self.centerydec
@@ -42,14 +42,13 @@ class Dude(Sprite):
 		centerxdec and centerydec, which are floats. These numbers
 		are then assigned to rect.centerx and rect.centery prior
 		to rendering to the screen """
-		self.newcenterxdec = self.centerxdec + (self.moving_x * 
+		self.newcenterxdec = self.centerxdec + (self.moving_x *
 				self.settings.dude_speed_factor)
-		self.newcenterydec = self.centerydec + (self.moving_y * 
+		self.newcenterydec = self.centerydec + (self.moving_y *
 				self.settings.dude_speed_factor)
-		if self.newcenterxdec > 40 and (self.newcenterxdec < 
-			self.settings.screen_width - 40): 
+		if self.newcenterxdec > 40 and (self.newcenterxdec <
+			self.settings.screen_width - 40):
 			self.centerxdec = self.newcenterxdec
-		if self.newcenterydec > 40 and (self.newcenterydec < 
+		if self.newcenterydec > 40 and (self.newcenterydec <
 			self.settings.screen_height -40):
 			self.centerydec = self.newcenterydec
-
